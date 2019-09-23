493f653a1ddcd85177dfbad401fad8c506caa046
chengchinlim <chengchin5227@gmail.com>
2019-09-23 13:43:31 -0700

    Add grid 4x4 (cell borders are individually constrained)

--- app/src/main/java/com/firematchesgame/MainActivity.kt
+++ app/src/main/java/com/firematchesgame/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.firematchesgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,21 @@ class MainActivity : AppCompatActivity() {
         val display = resources.displayMetrics
         Log.d(tag, "Width: " +  display.widthPixels) // 1080
         Log.d(tag, "Height: " + display.heightPixels) // 1998
+    }
+
+    fun h1Clicked(view: View) {
+        Toast.makeText(this, "h1 is clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun h2Clicked(view: View) {
+        Toast.makeText(this, "h2 is clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun h3Clicked(view: View) {
+        Toast.makeText(this, "h3 is clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun h4Clicked(view: View) {
+        Toast.makeText(this, "h4 is clicked", Toast.LENGTH_SHORT).show()
     }
 }
