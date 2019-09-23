fee4cfe409ead6606588c86bce6f8afae414d653
chengchinlim <chengchin5227@gmail.com>
2019-09-23 11:37:21 -0700

    Add horizontal and vertical dotted lines

--- app/src/main/java/com/firematchesgame/MainActivity.kt
+++ app/src/main/java/com/firematchesgame/MainActivity.kt
@@ -2,11 +2,18 @@ package com.firematchesgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "Main_Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val display = resources.displayMetrics
+        Log.d(tag, "Width: " +  display.widthPixels) // 1080
+        Log.d(tag, "Height: " + display.heightPixels) // 1998
     }
 }
